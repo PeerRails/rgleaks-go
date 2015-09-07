df7e776179f172038d1e7008043ed68b89c6afb8
Peer Rails <peer@omck.ws>
2015-09-07 18:11:11 +0300

    add new columns, add thumbnail creation, fix paths

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/go-xorm/xorm"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/nfnt/resize"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
 	"io"
 	"log"
 	"net/http"
@@ -19,15 +25,17 @@ type Images struct {
 	Id          int64
 	Source      string `xorm: "unique"`
 	Path        string
-	Name        string `xorm: "index"`
-	Uploaded_to string
-	Created_at  time.Time `xorm: "created"`
-	Updated_at  time.Time `xorm: "updated"`
+	Name        string `xorm: "index not null"`
+	Uploaded_to string `xorm: default 'none'`
+	Created_at  time.Time
+	Updated_at  time.Time
+	File_type   string `xorm: "index"`
+	Archived    bool
+	Thumb_path  string
 }
 
 var (
 	x       *xorm.Engine
-	imgbi   string = "https://img.bi"
 	sqlite  string = "./images.db"
 	psql    string = "dbname=images_test user=lenny password=123456 sslmode=disable"
 	img_dir string = "images/"
@@ -57,11 +65,12 @@ func ScrapeRgHost(url string) {
 func ScrapeFileLink(s *goquery.Selection) {
 	name := s.Text()
 	href, _ := s.Attr("href")
-	if IsImageType(name) {
+	isImage, ext := IsImageType(name)
+	if isImage {
 		url := GetDirectLink(href)
-		downloaded, fileName := DownloadImage(url, name)
+		downloaded, fileName, thumbName := DownloadImage(url, name, ext)
 		if downloaded {
-			err := InsertImage(&Images{Source: url, Path: fileName, Name: name})
+			err := InsertImage(&Images{Source: url, Path: fileName, Thumb_path: thumbName, Name: name, Created_at: time.Now(), Updated_at: time.Now(), Uploaded_to: "yes", File_type: ext})
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -69,13 +78,13 @@ func ScrapeFileLink(s *goquery.Selection) {
 	}
 }
 
-func IsImageType(name string) (result bool) {
+func IsImageType(name string) (result bool, ext_type string) {
 	split_name := strings.Split(name, ".")
 	ext := split_name[len(split_name)-1]
 	if ext == "jpg" || ext == "png" || ext == "gif" || ext == "jpeg" {
-		return true
+		return true, ext
 	} else {
-		return false
+		return false, ext
 	}
 }
 
@@ -83,7 +92,61 @@ func GetDirectLink(name string) (dl string) {
 	return fmt.Sprintf("http://rghost.ru%s/image.png", name)
 }
 
-func DownloadImage(url string, name string) (downloaded bool, fileName string) {
+func createThumb(fileName string, dirname string, ext string, nowTime string) (thumb string, err error) {
+
+	thumbDir := fmt.Sprintf("%s/thumb", dirname)
+	thumbDirCreated, err := dirExists(thumbDir)
+	if err != nil {
+		fmt.Println("Error while looking for", thumbDirCreated, "-", err)
+	}
+	if !thumbDirCreated {
+		err := os.Mkdir(thumbDir, 0777)
+		if err != nil {
+			fmt.Println("Error while creating", thumbDir, "-", err)
+			return thumbDir, err
+		}
+	}
+	filePath := fmt.Sprintf("%s/%s", dirname, fileName)
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error while opening", filePath, "-", err)
+		return fileName, err
+	}
+	var img image.Image
+	if ext == "jpeg" || ext == "jpg" {
+		img, err = jpeg.Decode(file)
+	} else if ext == "png" {
+		img, err = png.Decode(file)
+	} else if ext == "gif" {
+		img, err = gif.Decode(file)
+	} else {
+		err = errors.New("type is not supported")
+	}
+	if err != nil {
+		fmt.Println("Error while decoding", fileName, "-", err)
+		return fmt.Sprintf("images/%s/thumb/%s", nowTime, fileName), nil
+	}
+	file.Close()
+
+	m := resize.Thumbnail(200, 200, img, resize.NearestNeighbor)
+	thumbName := fmt.Sprintf("%s/%s", thumbDir, fileName)
+	out, err := os.Create(thumbName)
+	if err != nil {
+		return fmt.Sprintf("images/%s/thumb/%s", nowTime, fileName), err
+	}
+	defer out.Close()
+
+	if ext == "jpeg" || ext == "jpeg" {
+		jpeg.Encode(out, m, nil)
+	} else if ext == "png" {
+		png.Encode(out, m)
+	} else if ext == "gif" {
+		gif.Encode(out, m, nil)
+	}
+	return fmt.Sprintf("images/%s/thumb/%s", nowTime, fileName), err
+}
+
+func DownloadImage(url string, name string, ext string) (downloaded bool, fileName string, thumbName string) {
 	tokens := strings.Split(url, "/")
 	nowTime := time.Now().Format("20060102")
 	dirname := fmt.Sprintf("%s/%s", img_dir, nowTime)
@@ -95,32 +158,39 @@ func DownloadImage(url string, name string) (downloaded bool, fileName string) {
 		err := os.Mkdir(dirname, 0777)
 		if err != nil {
 			fmt.Println("Error while creating", dirname, "-", err)
-			return false, dirname
+			return false, "", ""
 		}
 	}
+
 	fileName = fmt.Sprintf("%s/%s-%s", dirname, tokens[len(tokens)-2], name)
 
 	output, err := os.Create(fileName)
 	if err != nil {
 		fmt.Println("Error while creating", fileName, "-", err)
-		return false, fileName
+		return false, "", ""
 	}
 	defer output.Close()
 
 	response, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Error while downloading", url, "-", err)
-		return false, fileName
+		return false, "", ""
 	}
 	defer response.Body.Close()
 
 	_, err = io.Copy(output, response.Body)
 	if err != nil {
 		fmt.Println("Error while downloading", url, "-", err)
-		return false, fileName
+		return false, "", ""
+	}
+
+	filePath := fmt.Sprintf("%s-%s", tokens[len(tokens)-2], name)
+	thumb, err := createThumb(filePath, dirname, ext, nowTime)
+	if err != nil {
+		thumb = fmt.Sprintf("images/%s/%s-%s", nowTime, tokens[len(tokens)-2], name)
 	}
 	fileName = fmt.Sprintf("images/%s/%s-%s", nowTime, tokens[len(tokens)-2], name)
-	return true, fileName
+	return true, fileName, thumb
 
 }
 
